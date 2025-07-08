ba61e3e070333aa02be55fc7a749bc93ed0f6d1c
jegorix <0311egor@gmail.com>
2025-07-08 22:15:22 +0300

    Consolidation of the material covered

---
+++ secondexamplebot/main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+from config import TOKEN
+import logging
+from app.handlers import router
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    dp.include_router(router)
+    await dp.start_polling(bot)
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nExit...\n")
